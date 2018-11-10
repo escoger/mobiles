f732222972de6a747126466143caab2cd0e6525d
gaurkuma <gaurav.kumar8@hpe.com>
2018-11-10 15:36:59 +0530

    gaurav changes

--- Escoger-Mobiles/src/main/java/com/escoger/mobiles/services/MobileService.java
+++ Escoger-Mobiles/src/main/java/com/escoger/mobiles/services/MobileService.java
@@ -3,17 +3,20 @@ package com.escoger.mobiles.services;
 import java.util.List;
 
 import com.escoger.mobiles.beans.AllMobileBean;
-import com.escoger.mobiles.repo.config.AllMobilesRepo;
 
 public interface MobileService {
-	
-	
+
 	public List<AllMobileBean> getAllMobiles();
+
 	public List<Object> getAllMobilesBasedOnBrand(String brand);
+
 	public List<Object> getAllMobilesBasedOnBrandAndNetworkType(String brand, String networktype);
+
 	public List<Object> getAllMobilesBrandBasedOnRecentLaunches(String brand, String recentLaunches);
+
 	public List<Object> getAllMobilesBrandBasedOnBasicPhones(String brand, String basicphones);
+
 	public List<Object> getAllMobilesBrandBasedOnDualSimphones(String brand, String dualsimphones);
+
 	public List<Object> getAllAndroidMobilesBasedOnBrand(String brand);
 }
-
