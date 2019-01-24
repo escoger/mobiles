cdf7e7ddab88c6a1c3c33396b7b5a4d528c5c06b
GAURAV KUMAR <escoger.co@gmail.com>
2019-01-25 00:30:12 +0530

    commit

--- Escoger-Mobiles/src/main/java/com/escoger/mobiles/controller/MobileController.java
+++ Escoger-Mobiles/src/main/java/com/escoger/mobiles/controller/MobileController.java
@@ -44,13 +44,56 @@ public class MobileController {
 		}
 		return new ResponseEntity<List<Object>>(mobService.getAllMobilesBasedOnBrand(brand), HttpStatus.OK);
 	}
+	@GetMapping	("mobiles/mobileBrands/{brands}")
+	public ResponseEntity<List<Object>> getAllMobilesBasedOnBrands(@PathVariable String brands) {
+
+		return new ResponseEntity<List<Object>>(mobService.getAllMobilesBasedOnBrands(brands), HttpStatus.OK);
+	}
+	
+	@GetMapping	("mobiles/mobilePrice/{price}")
+	public ResponseEntity<List<Object>> getAllMobilesBasedOnPrice(@PathVariable String price) {
+
+		return new ResponseEntity<List<Object>>(mobService.getAllMobilesBasedOnPrice(price), HttpStatus.OK);
+	}
+	
+	@GetMapping("mobilePrice/{price}/mobileBrand/{brand}")
+	public ResponseEntity<List<Object>> getAllMobilesBasedOnPriceAndBrand(@PathVariable String price, @PathVariable String brand) {
+		System.out.println("inside mobile controller");
+		return new ResponseEntity<List<Object>>(mobService.getAllMobilesBasedOnPriceAndBrand(price, brand), HttpStatus.OK);
+	}
 
 	@GetMapping("mobileBrand/{brand}/network/{networktype}")
 	public ResponseEntity<List<Object>> getAllMobilesBasedOnBrandAndNetworkType(@PathVariable String brand, @PathVariable String networktype) {
 		System.out.println("inside mobile controller");
 		return new ResponseEntity<List<Object>>(mobService.getAllMobilesBasedOnBrandAndNetworkType(brand, networktype), HttpStatus.OK);
 	}
+	
+	//netwrok type
+	@GetMapping("mobiles/network/{networktype}")
+	public ResponseEntity<List<Object>> getAllMobilesBasedOnNetworkType(@PathVariable String networktype) {
+		System.out.println("inside mobile controller");
+		return new ResponseEntity<List<Object>>(mobService.getAllMobilesBasedOnNetworkType(networktype), HttpStatus.OK);
+	}
+	
+	
+	// netwrok type and brand nd price
+		@GetMapping("mobiles/network/{networktype}/mobileBrand/{brand}/mobilePrice/{price}")
+		public ResponseEntity<List<Object>> getAllMobilesBasedOnNetworkTypeAndBrandAndPrice(@PathVariable String networktype,@PathVariable String brand,@PathVariable String price) {
+			System.out.println("inside mobile controller");
+			return new ResponseEntity<List<Object>>(mobService.getAllMobilesBasedOnNetworkTypeAndBrandAndPrice(networktype,brand,price), HttpStatus.OK);
+		}
+	
+	
+
+	
 	//recent launches
+	@GetMapping	("mobiles/recentLaunches/{recentLaunches}")
+	public ResponseEntity<List<Object>> getAllMobilesBrandBasedOOnRecentLaunches(@PathVariable String recentLaunches) {
+
+		return new ResponseEntity<List<Object>>(mobService.getAllMobilesBasedOnRecentLaunches(recentLaunches), HttpStatus.OK);
+	}
+	
+	//recent launches and brand
 	@GetMapping	("mobiles/mobileBrand/{brand}/recentLaunches/{recentLaunches}")
 	public ResponseEntity<List<Object>> getAllMobilesBrandBasedOOnRecentLaunches(@PathVariable String brand, @PathVariable String recentLaunches) {
 
@@ -68,12 +111,86 @@ public class MobileController {
 	@GetMapping	("mobiles/mobileBrand/{brand}/dualsimphones{dualsimphones}")
 	public ResponseEntity<List<Object>> getAllMobilesBrandBasedOnDualSimphones(@PathVariable String brand, @PathVariable String dualsimphones) {
 
-		return new ResponseEntity<List<Object>>(mobService.getAllMobilesBrandBasedOnDualSimphones(brand,dualsimphones), HttpStatus.OK);
-	}
+			return new ResponseEntity<List<Object>>(mobService.getAllMobilesBrandBasedOnDualSimphones(brand,dualsimphones), HttpStatus.OK);
+		}
+		
+		//os type
+				@GetMapping	("mobiles/OsType/{osType}")
+				public ResponseEntity<List<Object>> getAllMobilesBasedOnOsType(@PathVariable String osType) {
 
-	@GetMapping("mobiles/mobileBrand/{brand}/androidphones")
-	public ResponseEntity<List<Object>> getAllAndroidMobilesBasedOnBrand(@PathVariable String brand) {
+					return new ResponseEntity<List<Object>>(mobService.getAllMobilesBasedOnOsType(osType), HttpStatus.OK);
+				}
+				
+				//os type and brand
+				@GetMapping	("mobiles/OsType/{osType}/mobileBrand/{brand}")
+				public ResponseEntity<List<Object>> getAllMobilesBasedOnOsTypeAndBrand(@PathVariable String osType,@PathVariable String brand) {
+
+					return new ResponseEntity<List<Object>>(mobService.getAllMobilesBasedOnOsTypeAndBrand(osType,brand), HttpStatus.OK);
+				}
+				
+				//ram
+				@GetMapping	("mobiles/ram/{ram}")
+				public ResponseEntity<List<Object>> getAllMobilesBasedOnRam(@PathVariable String ram) {
+
+					return new ResponseEntity<List<Object>>(mobService.getAllMobilesBasedOnBrand(ram), HttpStatus.OK);
+				}
+				
+				//ram and brand
+				@GetMapping	("mobiles/ram/{ram}/mobileBrand/{brand}")
+				public ResponseEntity<List<Object>> getAllMobilesBasedOnRamAndBrand(@PathVariable String ram,@PathVariable String brand) {
+
+					return new ResponseEntity<List<Object>>(mobService.getAllMobilesBasedOnRamAndBrand(ram,brand), HttpStatus.OK);
+				}
+				
+				//offers
+				@GetMapping	("mobiles/offers/{offers}")
+				public ResponseEntity<List<Object>> getMobilesBasedOnOffers(@PathVariable String offers) {
+
+					return new ResponseEntity<List<Object>>(mobService.getMobilesBasedOnOffers(offers), HttpStatus.OK);
+				}
+				
+				
+				//brand and offers
+				
+				@GetMapping	("mobiles/mobileBrand/{brand}/offers/{offers}")
+				public ResponseEntity<List<Object>> getMobilesBasedOnBrandAndOffers(@PathVariable String brand,@PathVariable String offers) {
+
+					return new ResponseEntity<List<Object>>(mobService.getMobilesBasedOnBrandAndOffers(brand,offers), HttpStatus.OK);
+				}
+				
+                //camera and brand and price
+				
+				@GetMapping	("mobiles/camera/{camera}/mobileBrand/{brand}/mobilePrice/{price}")
+				public ResponseEntity<List<Object>> getMobilesBasedOnCameraAndBrandAndPrice(@PathVariable String camera,@PathVariable String brand,@PathVariable String price) {
+
+					return new ResponseEntity<List<Object>>(mobService.getMobilesBasedOnCameraAndBrandAndPrice(camera,brand,price), HttpStatus.OK);
+				}
+				
+				
+               //camera and brand
+				
+				@GetMapping	("mobiles/camera/{camera}/mobileBrand/{brand}")
+				public ResponseEntity<List<Object>> getMobilesBasedOnCameraAndBrand(@PathVariable String camera,@PathVariable String brand) {
+
+					return new ResponseEntity<List<Object>>(mobService.getMobilesBasedOnCameraAndBrand(camera,brand), HttpStatus.OK);
+				}
+             //camera
+				
+				@GetMapping	("mobiles/camera/{camera}")
+				public ResponseEntity<List<Object>> getMobilesBasedOnCamera(@PathVariable String camera) {
+
+					return new ResponseEntity<List<Object>>(mobService.getMobilesBasedOnCamera(camera), HttpStatus.OK);
+				}
+				
+				
+	  @GetMapping("mobiles/mobileBrand/{brand}/androidphones")
+	  public ResponseEntity<List<Object>> getAllAndroidMobilesBasedOnBrand(@PathVariable String brand) {
 		return new ResponseEntity<List<Object>>(mobService.getAllAndroidMobilesBasedOnBrand(brand), HttpStatus.OK);
+	}
+	  
+	  @GetMapping("mobiles/mobiledetails/{modelNo}")
+	  public ResponseEntity<List<Object>> getMobileDetails(@PathVariable String model_no) {
+		return new ResponseEntity<List<Object>>(mobService.getMobileDetails(model_no), HttpStatus.OK);
 	}
 
 }
