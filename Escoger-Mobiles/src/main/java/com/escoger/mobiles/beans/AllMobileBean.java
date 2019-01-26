dac6212ed248e55f73e6dc150360085637e57f32
GAURAV KUMAR <escoger.co@gmail.com>
2019-01-26 18:08:47 +0530

    commit

--- Escoger-Mobiles/src/main/java/com/escoger/mobiles/beans/AllMobileBean.java
+++ Escoger-Mobiles/src/main/java/com/escoger/mobiles/beans/AllMobileBean.java
@@ -19,15 +19,41 @@ public class AllMobileBean implements Serializable{
 	private String image_url;
 	private String price ;
 	private String best_price;
-	private String offer;
-	private String os;
 	
-	
-	
-	
-	
-	
-	
-	
-
+	public String getBrand() {
+		return brand;
+	}
+	public void setBrand(String brand) {
+		this.brand = brand;
+	}
+	public String getModel_number() {
+		return model_number;
+	}
+	public void setModel_number(String model_number) {
+		this.model_number = model_number;
+	}
+	public String getModel_Name() {
+		return model_Name;
+	}
+	public void setModel_Name(String model_Name) {
+		this.model_Name = model_Name;
+	}
+	public String getImage_url() {
+		return image_url;
+	}
+	public void setImage_url(String image_url) {
+		this.image_url = image_url;
+	}
+	public String getPrice() {
+		return price;
+	}
+	public void setPrice(String price) {
+		this.price = price;
+	}
+	public String getBest_price() {
+		return best_price;
+	}
+	public void setBest_price(String best_price) {
+		this.best_price = best_price;
+	}
 }
