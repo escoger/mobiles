f3e6ce6e7354201d43bb391f15c07073cc4369ac
GAURAV KUMAR <escoger.co@gmail.com>
2019-02-23 13:01:20 +0530

    Cross origin url change

--- Escoger-Mobiles/src/main/java/com/escoger/mobiles/controller/MobileController.java
+++ Escoger-Mobiles/src/main/java/com/escoger/mobiles/controller/MobileController.java
@@ -19,7 +19,7 @@ import com.escoger.mobiles.services.MobileService;
 
 @RestController
 @RequestMapping(path = "/mobiles")//, consumes = "application/json", produces = "application/json")
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "http://18.219.194.106:4200")
 public class MobileController {
 	private static final Logger logger = LoggerFactory.getLogger(MobileController.class);
 	MobileService mobService;
